d87a11712fe6d46c21869f58082728afac778b03
Dion <dionpimentel260@gmail.com>
2022-05-18 14:10:44 -0700

    Music Switching

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    AudioSource player;
+    [SerializeField]
+    private AudioClip normal;
+    [SerializeField]
+    private AudioClip chase;
+    public static BackgroundMusic instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GetComponent<AudioSource>();
+        if (instance != null && instance != this)
+            Destroy(this);
+        else
+            instance = this;
+    }
+
+    public void SwapMusic()
+    {
+        player.Stop();
+        if(player.clip == normal)
+        {
+            player.clip = chase;
+        }
+        else
+        {
+            player.clip = normal;
+        }
+        player.Play();
+    }
+}
